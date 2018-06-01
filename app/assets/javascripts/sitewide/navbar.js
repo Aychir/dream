ea2436122c38490305a769d3817589a8d914b236
Alex Chiriac <chiriac@Alexs-Air.columbus.rr.com>
2018-06-01 11:02:31 -0400

    More navbar work.

--- app/assets/javascripts/sitewide/navbar.js
+++ app/assets/javascripts/sitewide/navbar.js
@@ -9,3 +9,15 @@ $(window).resize(function() {
     'width': ($(".left-container").width() + 'px')
   });
 });
+
+$(document).ready(function() {
+	$('input[type="search"]').on('focus', function() {
+	    $('.right-container').hide();
+	});
+});
+
+$(document).ready(function() {
+	$('input[type="search"]').off('focus', function() {
+	    $('.right-container').show();
+	});
+});
