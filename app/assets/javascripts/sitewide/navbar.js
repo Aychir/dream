718dcd7e8dea1c39b84e3316d6877bd3f8e96c03
Alex Chiriac <chiriac@Alexs-Air.columbus.rr.com>
2018-06-02 14:48:49 -0400

    Further implementation of navbar.

--- app/assets/javascripts/sitewide/navbar.js
+++ app/assets/javascripts/sitewide/navbar.js
@@ -1,25 +1,52 @@
+//First two functions for ensuring the right half of the navbar is the same width as the left half so the search bar will be centered.
 $(document).ready(function() {
   $(".right-container").css({
     'width': ($(".left-container").width() + 'px')
   });
+
+  $(".btn-outline-secondary").css({
+    'height': ($("ul.nav").height() + 'px')
+  })
 });
 
 $(window).resize(function() {
  $(".right-container").css({
     'width': ($(".left-container").width() + 'px')
   });
+
+ $(".btn-outline-secondary").css({
+    'height': ($("ul.nav").height() + 'px')
+  })
 });
 
+//Collapsing of the left & right sides of navbar when screen too small
 $(function(){
-  $('input[type=search]').focus(function(){
-    $(".right-container").hide();
-  }).blur(function(){
-    $(".right-container").show();
-  });
+      $('input[type=search]').focus(function(){
+        if($(window).width() <= 648){
+          $(".right-container").hide(300);
+        }}).blur(function(){
+        if($(window).width() <= 648){
+          $(".right-container").show(300);
+        }});
 
-  $('input[type=search]').focus(function(){
-    $(".left-container").hide();
-  }).blur(function(){
-    $(".left-container").show();
-  });
+      $('input[type=search]').focus(function(){
+        if($(window).width() <= 648){
+          $(".left-container").hide(300);
+        }}).blur(function(){
+        if($(window).width() <= 648){
+          $(".left-container").show(300);
+        }});
+});
+
+$(window).resize(function(){
+  if($('input[type=search]').is(':focus')){
+    if($(window).width() <= 648){
+      $(".left-container").hide(300);
+      $(".right-container").hide(300);
+    }
+    else{
+      $(".left-container").show(300);
+      $(".right-container").show(300);
+    }
+  }
 });
