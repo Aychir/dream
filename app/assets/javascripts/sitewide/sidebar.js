54c1f460ca90939e192899142da8769ec44af9d5
Alex Chiriac <chiriac@Alexs-Air.columbus.rr.com>
2018-06-08 20:18:18 -0400

    Initial implementation of sidebar overlay.

--- app/assets/javascripts/sitewide/sidebar.js
+++ app/assets/javascripts/sitewide/sidebar.js
@@ -1,15 +1,19 @@
-$(document).ready(function () {
-
-    $('#sidebarCollapse').on('click', function () {
-        // open or close navbar
-        $('#sidebar').toggleClass('active');
-        $('#content').toggleClass('active');
-    });
-
-});
-
 $(function() {
     $(".panel-collapse").on("click", function(e) {
         e.stopPropagation();
     });
+});
+
+$(function(){
+	$('#sidebarCollapse').on('click', function(){
+		if($(window).width() > 768){
+			$('#sidebar').toggleClass('active');
+        	$('#content').toggleClass('active');
+		}
+		else{
+			$('body').toggleClass('overlay');
+			$('#sidebar').toggleClass('sidebarOverlay');
+			$('.col-12 td').toggleClass('noClick');
+		}
+	});
 });
