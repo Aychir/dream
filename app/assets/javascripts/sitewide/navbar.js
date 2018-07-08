193b41a53bb1f8c2b8386ee910eda97226eb6e32
Aychir <Aychir@github.com>
2018-07-08 14:57:46 -0400

    Initial implementation of the revamped search form.

--- app/assets/javascripts/sitewide/navbar.js
+++ app/assets/javascripts/sitewide/navbar.js
@@ -1,60 +1,80 @@
 //First two functions for ensuring the right half of the navbar is the same width as the left half so the search bar will be centered.
+// $(document).ready(function() {
+//   $(".right-container").css({
+//     'width': ($(".left-container").width() + 'px')
+//   });
+
+//   $(".btn-outline-secondary").css({
+//     'height': ($("input.form-control").outerHeight() + 'px')
+//   })
+
+//   $("#content").css({
+//     'padding-top': ($("nav.navbar").height() + 16 + 'px')
+//   });
+// });
+
+// $(window).resize(function() {
+//  $(".right-container").css({
+//     'width': ($(".left-container").width() + 'px')
+//   });
+
+//  $(".btn-outline-secondary").css({
+//     'height': ($("input.form-control").outerHeight() + 'px')
+//   });
+
+//  $("#content").css({
+//     'padding-top': ($("nav.navbar").height() + 16 + 'px')
+//   });
+// });
+
 $(document).ready(function() {
-  $(".right-container").css({
-    'width': ($(".left-container").width() + 'px')
-  });
-
-  $(".btn-outline-secondary").css({
-    'height': ($("input.form-control").outerHeight() + 'px')
-  })
-
-  $("#content").css({
-    'padding-top': ($("nav.navbar").height() + 16 + 'px')
-  });
+  $("button.search").css({
+     'height': ($("input.searchBox").outerHeight() + 'px')
+   });
 });
 
-$(window).resize(function() {
- $(".right-container").css({
-    'width': ($(".left-container").width() + 'px')
-  });
-
- $(".btn-outline-secondary").css({
-    'height': ($("input.form-control").outerHeight() + 'px')
-  });
-
- $("#content").css({
-    'padding-top': ($("nav.navbar").height() + 16 + 'px')
-  });
+$(document).ready(function(){
+	$("#searchForm").hide();
 });
+
+
+$(function(){
+	$('#searchIconButton').on('click', function(){
+		$("#searchForm").css({
+			'display':("block")
+		});
+	});
+});
+
 
 //Collapsing of the left & right sides of navbar when screen too small
-$(function(){
-      $('input[type=search]').focus(function(){
-        if($(window).width() <= 648){
-          $(".right-container").hide(350);
-        }}).blur(function(){
-        if($(window).width() <= 648){
-          $(".right-container").show(410);
-        }});
+// $(function(){
+//       $('input[type=search]').focus(function(){
+//         if($(window).width() <= 648){
+//           $(".right-container").hide(350);
+//         }}).blur(function(){
+//         if($(window).width() <= 648){
+//           $(".right-container").show(410);
+//         }});
 
-      $('input[type=search]').focus(function(){
-        if($(window).width() <= 648){
-          $(".left-container").hide(350);
-        }}).blur(function(){
-        if($(window).width() <= 648){
-          $(".left-container").show(350);
-        }});
-});
+//       $('input[type=search]').focus(function(){
+//         if($(window).width() <= 648){
+//           $(".left-container").hide(350);
+//         }}).blur(function(){
+//         if($(window).width() <= 648){
+//           $(".left-container").show(350);
+//         }});
+// });
 
-$(window).resize(function(){
-  if($('input[type=search]').is(':focus')){
-    if($(window).width() <= 648){
-      $(".left-container").hide(300);
-      $(".right-container").hide(300);
-    }
-    else{
-      $(".left-container").show(300);
-      $(".right-container").show(410);
-    }
-  }
-});
+// $(window).resize(function(){
+//   if($('input[type=search]').is(':focus')){
+//     if($(window).width() <= 648){
+//       $(".left-container").hide(300);
+//       $(".right-container").hide(300);
+//     }
+//     else{
+//       $(".left-container").show(300);
+//       $(".right-container").show(410);
+//     }
+//   }
+// });
