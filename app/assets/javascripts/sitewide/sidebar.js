e9557ba6804f73e3129b5eb8971edee671dba774
Aychir <Aychir@github.com>
2018-08-02 19:20:14 -0400

    Got about section to work as intended.

--- app/assets/javascripts/sitewide/sidebar.js
+++ app/assets/javascripts/sitewide/sidebar.js
@@ -41,13 +41,23 @@ $(function(){
 	});
 });
 
-$(document).ready(function() {
-  $('#sidebarList').toggleClass('noShow');
-});
-
+/*
+	This function will toggle class bottomZero (initially used) because bottom: 0 is set in there
+	and this will allow the only relative ancestor to be the body, so it will position at the bottom
+	of the sidebar. When clicking the link, we first expand/collapse the list for transition purposes
+	and toggle bottomZero, this takes off the bottome attribute and allows the element to overflow to 
+	the bottom of the document- in its original position because it has no TLBR attributes set.
+*/
 $(function(){
-	$('.panel-heading').on('click', function(){
-		$('#sidebarList').toggleClass('noShow');
-		$('#sidebar').toggleClass('fullHeight');
+	$('.panel-title a').on('click', function(){
+		//Do this because the timing of the about section was off when collapsing collapse1
+		if($("#collapse1").css('display') == 'none'){
+			$("#collapse1").css('display', 'block');
+		}
+		else{
+	 		//We want to make it do nothing until the display is none
+	 		$("#collapse1").css('display', 'none');
+		}
+		$('.about-section').toggleClass('bottomZero');
 	});
 });
