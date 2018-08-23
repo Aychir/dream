877dc81737294f1699ad86b0ea552bf4d8647c19
Aychir <Aychir@github.com>
2018-08-23 01:16:11 -0400

    Initial implementation of video uploads.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,13 @@
 class Post < ApplicationRecord
 	has_one_attached :image
+
+	validate :correct_image_type
+
+	private
+
+	def correct_image_type
+		if image.attached? && !image.content_type.in?(%w(image/jpeg image/jpg image/png image/gif video/mp4))
+			self.errors.add(:image, 'must be a JPEG, JPG or a PNG file')
+		end
+	end
 end
