1287182e908c572010638438309ac09bb02c15b4
Aychir <Aychir@github.com>
2018-09-03 23:35:32 -0400

    Settings button refinements.

--- app/assets/javascripts/sitewide/dropdown.js
+++ app/assets/javascripts/sitewide/dropdown.js
@@ -1,13 +1,12 @@
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-    console.log("CLICKED");
+function postDropdownFunction() {
+    document.getElementById("settings-dropdown").classList.toggle("show");
 }
 
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn') && !event.target.matches('.fa-ellipsis-v')) {
+  if (!event.target.matches('#settings-button') && !event.target.matches('.fa-ellipsis-v')) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
