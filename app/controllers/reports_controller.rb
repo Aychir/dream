33280841762d9f3d23c3663c7df1685f58c2f124
Aychir <Aychir@github.com>
2018-09-06 23:48:59 -0400

    Reporting posts and fixed dropdown.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -21,8 +21,11 @@ class ReportsController < ApplicationController
     else
       @userCheck = false
       @post = Post.find(params[:format])
-      @report = Report.new
-      #here need to check if user has already reported the post
+      if(!current_user_has_reported(current_user.id, @post.id))
+        @report = Report.new
+      else
+        redirect_to users_path, notice: 'You cannot report this post again...'
+      end
     end
   end
 
@@ -30,10 +33,12 @@ class ReportsController < ApplicationController
       @report = Report.new(report_params)     
         respond_to do |format|
           if @report.save
+            puts "HI"
             format.html { redirect_to users_path, notice: 'Report submitted.' }
             format.json { render :show, status: :created, location: @report.user_id }
           else
-            format.html { render :new, notice: 'Report not submitted! Please try again.'}
+            puts "RIP"
+            format.html { redirect_to new_report_path, notice: 'Report not submitted! Please try again.'}
             format.json { render json: @report.errors, status: :unprocessable_entity }
           end
         end
