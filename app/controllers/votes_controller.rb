13e6d17f1f737813e54775eab3dade59e7664e47
Aychir <Aychir@github.com>
2018-09-10 00:59:00 -0400

    Can now un-upvote!

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,15 +2,15 @@ class VotesController < ApplicationController
 
 	before_action :set_post, only: [:destroy]
 
-	#before_action :require_login, :only => [ :destroy]
-
   def create
     @vote = Vote.new(vote_params)
+    @post_id = params[:vote][:post_id]
 
     if @vote.save
       respond_to do |format|
         format.html { redirect_to users_path }
-        format.js # we'll use this later for AJAX!
+        #The response is the create.js.erb file in the votes view
+        format.js
       end
     else
     	@vote.destroy
@@ -18,9 +18,13 @@ class VotesController < ApplicationController
   end
 
   def destroy
-  	puts "destroy"
-  	if !@vote.destroy
-  		redirect_to users_path, :notice => "We were unable to process this request at this moment in time."
+    @post_id = params[:vote][:post_id]
+  	if @vote.destroy
+  		respond_to do |format|
+        format.html { redirect_to users_path }
+        #The response is the destroy.js.erb file in the votes view
+        format.js
+      end
   	end
   end
 
@@ -34,4 +38,13 @@ class VotesController < ApplicationController
 	def vote_params
 		params.require(:vote).permit(:user_id, :post_id)
 	end
+end
+
+=begin
+  
+So what's next?
+-Implement voting for downvotes as well
+-Differentiate between voting types
+-Change implementation to reflect change in voting type
+-Show when the post is upvoted and have a delete form instead of create form ready when page is loaded 
 end
