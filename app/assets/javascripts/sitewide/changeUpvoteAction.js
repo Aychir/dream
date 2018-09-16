a942274f38c37488dd6cfef317030596435f8586
Aychir <Aychir@github.com>
2018-09-16 01:13:32 -0400

    Full implementation of voting system (I think).

--- app/assets/javascripts/sitewide/changeUpvoteAction.js
+++ app/assets/javascripts/sitewide/changeUpvoteAction.js
@@ -5,3 +5,7 @@ function changeUpvoteAction(post_id, vote_id){
 function changeDownvoteAction(post_id, vote_id){
 	$("#the-downvote-form").attr("action", "/posts/" + post_id + "/votes/" + vote_id);
 }
+
+function changeToUpdateUpvote(post_id, vote_id){
+	$("#the-upvote-form").attr("action", "/posts/" + post_id + "/votes/" + vote_id + "/update_to_upvote");
+}
