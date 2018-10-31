7040964be8fbe501029a0d5e2ec641a0e970a6dd
Aychir <Aychir@github.com>
2018-10-31 00:56:53 -0400

    Showing hashtags now animated.

--- app/assets/javascripts/sitewide/hashtag-tab.js
+++ app/assets/javascripts/sitewide/hashtag-tab.js
@@ -1,11 +1,7 @@
 $(function(){
 	$('#bottom-tab-icon').on('click', function(){
-		$("#bottom-tab").fadeToggle("slow", function () {
-			$("#bottom-tab").toggleClass("expanded");
-		});
+		$("#bottom-tab").toggleClass("expanded");
 
-		$("#hidden-tab-container").fadeToggle("slow", function () {
-			$("#hidden-tab-container").toggleClass("expanded");
-		});
+		$("#hidden-tab-container").toggleClass("expanded");
 	});
 });
